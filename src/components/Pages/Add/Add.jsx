7dd99854dab98c2de2e59f9c6179b5de8f919abc
Ali Kamran <43109679+alikamranch@users.noreply.github.com>
2020-04-13 13:38:06 +0500

    add page completed

--- src/components/Pages/Add/Add.jsx
+++ src/components/Pages/Add/Add.jsx
@@ -2,40 +2,41 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import firebase from "../../firebase";
 import "./Add.css";
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from "mdbreact";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+} from "mdbreact";
 
 class Add extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       allData: [],
+      weeklyStats: {},
+      overallStats: {},
       people: {
         id: null,
         contact: "",
         name: "",
         status: "",
       },
-      overall_stats: {
-        totalBagsDispatched: null,
-        totalDonationsCollected: null,
-        totalDonationsRs: null,
-        totalFamiliesServed: null,
-      },
-      weekly_stats: {
-        newCases: null,
-        totalBagsDispatched: null,
-        totalDonationsCollected: null,
-        totalDonationsRs: null,
-        totalFamiliesServed: null,
-      },
+      overall_stats: {},
+      weekly_stats: {},
     };
   }
 
   //Handles fetching data from firebase
   componentDidMount() {
     const db = firebase.firestore();
-    var docRef = db.collection("pukaar").doc("families");
 
+    //for families document
+    var docRef = db.collection("pukaar").doc("families");
     docRef
       .get()
       .then((doc) => {
@@ -44,6 +45,44 @@ class Add extends React.Component {
           allData: data,
         });
         console.log(this.state.allData);
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+
+    //for weekly stats document
+    var docRef2 = db.collection("pukaar").doc("weekly_stats");
+    docRef2
+      .get()
+      .then((doc) => {
+        const data = doc.data().stats;
+        this.setState({
+          weeklyStats: data,
+        });
+        const weekly = this.state.weeklyStats;
+        this.setState({
+          weekly_stats: weekly,
+        });
+        console.log(this.state.weeklyStats);
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
+
+    //for overall stats document
+    var docRef3 = db.collection("pukaar").doc("overall_stats");
+    docRef3
+      .get()
+      .then((doc) => {
+        const data = doc.data().stats;
+        this.setState({
+          overallStats: data,
+        });
+        const overall = this.state.overallStats;
+        this.setState({
+          overall_stats: overall,
+        });
+        console.log(this.state.overallStats);
       })
       .catch(function (error) {
         console.log("Error getting document:", error);
@@ -100,8 +139,8 @@ class Add extends React.Component {
   //change handler for weekly stats form
   handleWeeklyStats = (e) => {
     this.setState({
-      weekly_stats: {
-        ...this.state.weekly_stats,
+      weeklyStats: {
+        ...this.state.weeklyStats,
         [e.target.name]:
           e.target.type === "number"
             ? parseInt(e.target.value, 10)
@@ -112,7 +151,7 @@ class Add extends React.Component {
 
   //submit handler for the Update Weekly Stats form
   handleWeeklyStatsSubmit = (event) => {
-    const weeklyStats = this.state.weekly_stats;
+    const weeklyStats = this.state.weeklyStats;
     console.log(weeklyStats);
     const db = firebase.firestore();
     db.collection("pukaar")
@@ -126,22 +165,32 @@ class Add extends React.Component {
       });
     alert("Weekly Stats Updated!");
     event.preventDefault();
-    this.setState({
-      weekly_stats: {
-        newCases: "",
-        totalBagsDispatched: "",
-        totalDonationsCollected: "",
-        totalDonationsRs: "",
-        totalFamiliesServed: "",
-      },
-    });
+
+    //for weekly stats document
+    var docRef2 = db.collection("pukaar").doc("weekly_stats");
+    docRef2
+      .get()
+      .then((doc) => {
+        const data = doc.data().stats;
+        this.setState({
+          weeklyStats: data,
+        });
+        const weekly = this.state.weeklyStats;
+        this.setState({
+          weekly_stats: weekly,
+        });
+        console.log(this.state.weeklyStats);
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
   };
 
   //change handler for overall stats form
   handleOverallStats = (e) => {
     this.setState({
-      overall_stats: {
-        ...this.state.overall_stats,
+      overallStats: {
+        ...this.state.overallStats,
         [e.target.name]:
           e.target.type === "number"
             ? parseInt(e.target.value, 10)
@@ -152,7 +201,7 @@ class Add extends React.Component {
 
   //submit handler for the Update Overall Stats form
   handleOverallStatsSubmit = (event) => {
-    const overallStats = this.state.overall_stats;
+    const overallStats = this.state.overallStats;
     console.log(overallStats);
     const db = firebase.firestore();
     db.collection("pukaar")
@@ -166,23 +215,36 @@ class Add extends React.Component {
       });
     alert("Overall Stats Updated!");
     event.preventDefault();
-    this.setState({
-      overall_stats: {
-        totalBagsDispatched: "",
-        totalDonationsCollected: "",
-        totalDonationsRs: "",
-        totalFamiliesServed: "",
-      },
-    });
+
+    //for overall stats document
+    var docRef3 = db.collection("pukaar").doc("overall_stats");
+    docRef3
+      .get()
+      .then((doc) => {
+        const data = doc.data().stats;
+        this.setState({
+          overallStats: data,
+        });
+        const overall = this.state.overallStats;
+        this.setState({
+          overall_stats: overall,
+        });
+        console.log(this.state.overallStats);
+      })
+      .catch(function (error) {
+        console.log("Error getting document:", error);
+      });
   };
 
   render() {
+    const weekly = this.state.weekly_stats;
+    const overall = this.state.overall_stats;
     return (
       <div className="mb-5">
         <Helmet>
           <title>Add</title>
         </Helmet>
-        <div className="mt-5">
+        <div className="mt-5 mb-5">
           <MDBContainer>
             <h3 className="text-left">Add New People</h3>
             <hr></hr>
@@ -227,6 +289,7 @@ class Add extends React.Component {
               </MDBRow>
             </MDBContainer>
           </MDBContainer>
+          <hr className="home-hr-style" />
         </div>
 
         <div className="mt-5">
@@ -235,41 +298,66 @@ class Add extends React.Component {
             <hr></hr>
             <MDBContainer>
               <MDBRow className="justify-content-center">
+                <h5>Current</h5>
+                <br />
+                <br />
+                <MDBTable responsive>
+                  <MDBTableHead color="primary-color" textWhite>
+                    <tr>
+                      <th>Total Families Served</th>
+                      <th>Total Donations Collected</th>
+                      <th>Total Bags Dispatched</th>
+                      <th>Total Donation Collected (RS)</th>
+                      <th>New Cases</th>
+                    </tr>
+                  </MDBTableHead>
+                  <MDBTableBody>
+                    <tr>
+                      <td>{weekly.totalFamiliesServed}</td>
+                      <td>{weekly.totalDonationsCollected}</td>
+                      <td>{weekly.totalBagsDispatched}</td>
+                      <td>{weekly.totalDonationsRs}</td>
+                      <td>{weekly.newCases}</td>
+                    </tr>
+                  </MDBTableBody>
+                </MDBTable>
+              </MDBRow>
+              <MDBRow className="justify-content-center">
                 <MDBCol md="4">
                   <form onSubmit={this.handleWeeklyStatsSubmit}>
                     <div className="grey-text">
                       <MDBInput
                         label="Total Families Served"
                         type="number"
-                        value={this.state.weekly_stats.totalFamiliesServed}
+                        value={this.state.weeklyStats.totalFamiliesServed}
                         name="totalFamiliesServed"
                         onChange={this.handleWeeklyStats}
                       />
                       <MDBInput
                         label="Total Donations Collected"
                         type="number"
-                        value={this.state.weekly_stats.totalDonationsCollected}
+                        value={this.state.weeklyStats.totalDonationsCollected}
                         name="totalDonationsCollected"
                         onChange={this.handleWeeklyStats}
                       />
                       <MDBInput
                         label="Total Bags Dispatched"
                         type="number"
-                        value={this.state.weekly_stats.totalBagsDispatched}
+                        value={this.state.weeklyStats.totalBagsDispatched}
                         name="totalBagsDispatched"
                         onChange={this.handleWeeklyStats}
                       />
                       <MDBInput
                         label="Total Donation Collected (Rs)"
                         type="number"
-                        value={this.state.weekly_stats.totalDonationsRs}
+                        value={this.state.weeklyStats.totalDonationsRs}
                         name="totalDonationsRs"
                         onChange={this.handleWeeklyStats}
                       />
                       <MDBInput
                         label="New Cases"
                         type="number"
-                        value={this.state.weekly_stats.newCases}
+                        value={this.state.weeklyStats.newCases}
                         name="newCases"
                         onChange={this.handleWeeklyStats}
                       />
@@ -284,6 +372,7 @@ class Add extends React.Component {
               </MDBRow>
             </MDBContainer>
           </MDBContainer>
+          <hr className="home-hr-style" />
         </div>
 
         <div className="mt-5">
@@ -292,34 +381,57 @@ class Add extends React.Component {
             <hr></hr>
             <MDBContainer>
               <MDBRow className="justify-content-center">
+                <h5>Current</h5>
+                <br />
+                <br />
+                <MDBTable responsive>
+                  <MDBTableHead color="secondary-color" textWhite>
+                    <tr>
+                      <th>Total Families Served</th>
+                      <th>Total Donations Collected</th>
+                      <th>Total Bags Dispatched</th>
+                      <th>Total Donation Collected (RS)</th>
+                    </tr>
+                  </MDBTableHead>
+                  <MDBTableBody>
+                    <tr>
+                      <td>{overall.totalFamiliesServed}</td>
+                      <td>{overall.totalDonationsCollected}</td>
+                      <td>{overall.totalBagsDispatched}</td>
+                      <td>{overall.totalDonationsRs}</td>
+                    </tr>
+                  </MDBTableBody>
+                </MDBTable>
+              </MDBRow>
+              <MDBRow className="justify-content-center">
                 <MDBCol md="4">
                   <form onSubmit={this.handleOverallStatsSubmit}>
                     <div className="grey-text">
                       <MDBInput
                         label="Total Families Served"
                         type="number"
-                        value={this.state.overall_stats.totalFamiliesServed}
+                        value={this.state.overallStats.totalFamiliesServed}
                         name="totalFamiliesServed"
                         onChange={this.handleOverallStats}
                       />
                       <MDBInput
                         label="Total Donations Collected"
                         type="number"
-                        value={this.state.overall_stats.totalDonationsCollected}
+                        value={this.state.overallStats.totalDonationsCollected}
                         name="totalDonationsCollected"
                         onChange={this.handleOverallStats}
                       />
                       <MDBInput
                         label="Total Bags Dispatched"
                         type="number"
-                        value={this.state.overall_stats.totalBagsDispatched}
+                        value={this.state.overallStats.totalBagsDispatched}
                         name="totalBagsDispatched"
                         onChange={this.handleOverallStats}
                       />
                       <MDBInput
                         label="Total Donation Collected (Rs)"
                         type="number"
-                        value={this.state.overall_stats.totalDonationsRs}
+                        value={this.state.overallStats.totalDonationsRs}
                         name="totalDonationsRs"
                         onChange={this.handleOverallStats}
                       />
